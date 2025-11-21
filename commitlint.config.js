c022d33d3825e1737d84f1a3acf23c5c49ccad20
Harsh <32413211+exelinguy@users.noreply.github.com>
2025-11-21 14:43:56 +0530

    :construction_worker: build: Configured linting for code and comments

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+    extends: ['gitmoji'],
+    rules: {
+        'subject-max-length': [2, 'always', 72], // Enforces a maximum subject length of 72 characters
+    },
+}
