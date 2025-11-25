749b626a86965729e1eb214cc7eb2fdccb70a480
Harsh <32413211+exelinguy@users.noreply.github.com>
2025-11-25 23:12:30 +0530

    👷 Build: Update commitlint config to improve Gitmoji compatibility

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,33 @@ module.exports = {
     extends: ['gitmoji'],
     rules: {
         'subject-max-length': [2, 'always', 72], // Enforces a maximum subject length of 72 characters
+        'subject-case': [2, 'always', ['lower-case', 'sentence-case']],
+        'type-case': [2, 'always', ['lower-case', 'sentence-case']],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'chore',
+                'style',
+                'refactor',
+                'test',
+                'perf',
+                'ci',
+                'build',
+                'Feat',
+                'Fix',
+                'Docs',
+                'Chore',
+                'Style',
+                'Refactor',
+                'Test',
+                'Perf',
+                'Ci',
+                'Build',
+            ],
+        ],
     },
 }
